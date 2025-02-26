cab44015ae56c5f9f0a187f0a438d68425ef223c
praveenzsp <psai6645@gmail.com>
2025-02-27 00:00:38 +0530

    added update, delete endpoint

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -35,3 +35,54 @@ export async function POST(request: Request) {
         );
     }
 }
+
+export async function PUT(request: Request) {
+    try {
+        const body = await request.json();
+        const todo = await prisma.todo.update({
+            where: {
+                id: body.id
+            },
+            data: {
+                title: body.title,
+                expiresAt: body.expiresAt
+            }
+        });
+        return NextResponse.json(todo);
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Failed to update todo" + error },
+            { status: 500 }
+        );
+    }
+}
+
+export async function DELETE(request: Request) {
+    try {
+        // Example URL: /api/todos?id=123e4567-e89b-12d3-a456-426614174000
+        const { searchParams } = new URL(request.url); // searchParam nothing but query params
+        const id = searchParams.get('id');
+
+        if (!id) {
+            return NextResponse.json(
+                { error: "Todo ID is required" },
+                { status: 400 }
+            );
+        }
+
+        const todo = await prisma.todo.delete({
+            where: {
+                id: id
+            }
+        });
+
+        return NextResponse.json(todo);
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Failed to delete todo" + error },
+            { status: 500 }
+        );
+    }
+}
+
+
