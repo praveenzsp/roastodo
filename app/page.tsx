74444d78d52e78270a0679e90d0d2f16927a7a56
praveenzsp <psai6645@gmail.com>
2025-03-05 21:51:06 +0530

    fixed auth bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { auth } from "@/auth";
 import Hero from "@/components/Hero";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+    const session  = await auth();
+    if (!session) {
+        redirect("/signin")
+    }
     return <Hero />;
 }
